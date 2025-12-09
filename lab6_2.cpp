5a9b7fbb1560647f4fe8a37cd90851b041ccb1bd
visionalee <flook.xtnt@gmail.com>
2025-12-09 16:40:23 +0700

    lab6_2.cpp completed

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
-
+#define _USE_MATH_DEFINES
+#include <cmath>
 using namespace std;
+
+double deg2rad(double a){
+    double calcu_a;
+    calcu_a = a*(M_PI/180);
+    return calcu_a;
+}
+
+double rad2deg(double a){
+    double calcu_a;
+    calcu_a = a*(180/M_PI);
+    return calcu_a;
+}
+double findXComponent(double line1,double line2,double angle1,double angle2){
+    double comp_x, x1,x2;
+    x1 = line1*cos(angle1);
+    x2 = line2*cos(angle2);
+    comp_x = x1+x2;
+    return comp_x;
+}
+
+double findYComponent(double line1,double line2,double angle1,double angle2){
+    double comp_y, y1,y2;
+    y1 = line1*sin(angle1);
+    y2 = line2*sin(angle2);
+    comp_y = y1+y2;
+    return comp_y;
+}
+
+double pythagoras(double comp1,double comp2) {
+    double ptg;
+    ptg = sqrt(pow(comp1, 2) + pow(comp2, 2));
+    return ptg;
+}
+
+void showResult(double l, double d){
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" << "\n";
+    cout << "Length of the resultant vector = " << l << "\n";
+    cout << "Direction of the resultant vector (deg) = " << d << "\n";
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";
+}
 
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
