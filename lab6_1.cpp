69c532a1ccd00802d6941384abb596a6d4c1a3ac
visionalee <flook.xtnt@gmail.com>
2025-12-09 16:39:10 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int num, odd_count = 0, even_count = 0;
+
+    do {
+        cout << "Enter an integer: ";
+        cin >> num;
+        
+        if (num != 0) {
+            if (num % 2 == 0) {
+                even_count++;
+            } else {
+                odd_count++;
+            }
+        }
+    } while (num != 0);
+
+    cout << "#Even numbers = " << even_count << "\n";
+    cout << "#Odd numbers = " << odd_count;
     return 0;
 }
